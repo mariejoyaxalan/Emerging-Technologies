b14044a8cf1cac7938c67d9597f144d9608dc0ff
austria_axalan <1902779@ub.edu.ph>
2021-09-26 15:15:37 +0000

    This the third and last part of our code.

--- lab2.py
+++ lab2.py
@@ -8,8 +8,19 @@ sfin = float(input("Input your semi-final grades: "))
 fin = float(input("Input your final grades: "))
 avg = (pre + mid + sfin + fin) / 4
 print(" ")
-print("Your total average is {}" .format (avg))
-if avg >= 75:
-  print("==PASSED==")
+if avg >= 99 and avg <= 100:
+    print("Your total average is {}" .format(avg) + ". \nYour grade is A. \nCongratulations, you PASSED!")
+elif avg >= 90 and avg <= 98:
+    print("Your total average is {}" .format(avg) + ". \nYour grade is B. \nCongratulations, you PASSED!")
+elif avg >= 80 and avg <= 89:
+    print("Your total average is {}" .format(avg) + ". \nYour grade is C. \nCongratulations, you PASSED!")
+elif avg >= 75 and avg <= 79:
+    print("Your total average is {}" .format(avg) + ". \nYour grade is D. \nCongratulations, you PASSED!")
+elif avg >= 70 and avg <= 74:
+    print("Your total average is {}" .format(avg) + ". \nYour grade is D. \nUnfortunately, you FAILED.")
+elif avg >= 61 and avg <= 69:
+    print("Your total average is {}" .format(avg) + ". \nYour grade is E. \nUnfortunately, you FAILED.")
+elif avg < 60:
+    print("Your total average is {}" .format(avg) + ". \nYour grade is F. \nUnfortunately, you FAILED.")
 else:
-  print("==FAILED==")
+    print("Invalid input!")
