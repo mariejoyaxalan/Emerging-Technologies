2d144c54373a7ee10c1aae1f036b7c6cdbfaf6c4
austria_axalan <1902779@ub.edu.ph>
2021-09-26 15:05:12 +0000

    This is the first part of our code.

---
+++ lab2.py
@@ -0,0 +1,11 @@
+print("----------------------------------------------")
+print("Emerging Technologies - Laboratory Activity 2")
+print("----------------------------------------------")
+print("")
+pre = float(input("Input your prelim grades: "))
+mid = float(input("Input your midterm grades: "))
+sfin = float(input("Input your semi-final grades: "))
+fin = float(input("Input your final grades: "))
+avg = (pre + mid + sfin + fin) / 4
+print(" ")
+print("Your total average is {}" .format (avg))
